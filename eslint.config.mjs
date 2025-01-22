dfcc8eab03593912feae7506e4a75a90dda847ef
davi.montalvao.ext <davi.montalvao.ext@ultragaz.com.br>
2025-01-22 09:44:34 -0300

    Segundo commit - site finalizado

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,13 @@ const compat = new FlatCompat({
 
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ['next'],
     rules: {
-      "@typescript-eslint/no-explicit-any": "error"
-    }
-  }
-];
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
